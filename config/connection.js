6234354f226612363b8a6fb1f5ddda9a02f8d334
skiplodem <hughesrva@gmail.com>
2019-02-27 21:19:30 -0500

    adding db changes

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "pwcspfbyl73eccbn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "kimwelaaom68lk7u",
-  password: "kbw5p0cyqqfwnzw7",
-  database: "ew8ku8gudvlkwicc"
-});
-
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Asaavd101",
+    database: "burgers_db"
+  });
+}
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
