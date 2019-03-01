f13a1c6b5475f4318c587db89fd2bfeb28054db5
skiplodem <hughesrva@gmail.com>
2019-02-28 21:09:26 -0500

    adding db changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+use ew8ku8gudvlkwicc;
 
 create table burgers(
-id int NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name varchar (10) not null,
 devoured boolean default false,
+createdAt TIMESTAMP NOT NULL default current_timestamp,
 primary key (id)
 );
